b0674d9c0acec3411d61ec363e5991a2e9d00ace
Yassine Essid <146223279+YassineEssid@users.noreply.github.com>
2025-02-06 19:17:34 +0100

    Update highlight.directive.spec.ts

--- src/app/highlight.directive.spec.ts
+++ src/app/highlight.directive.spec.ts
@@ -1,8 +1,14 @@
 import { HighlightDirective } from './highlight.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    // Mock ElementRef and Renderer2
+    const elementRefMock = {} as ElementRef;
+    const rendererMock = {} as Renderer2;
+
+    // Provide the mocked dependencies
+    const directive = new HighlightDirective(elementRefMock, rendererMock);
     expect(directive).toBeTruthy();
   });
 });
